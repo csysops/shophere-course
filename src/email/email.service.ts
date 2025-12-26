388a1fead160043255276d5122f07034180dee5e
Pham Dat <122384368+csysops@users.noreply.github.com>
2025-12-26 08:35:11 +0700

    Update email.service.ts

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,34 +1,53 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
+import * as fs from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class EmailService {
   constructor(private mailerService: MailerService) {}
 
   // Gửi email xác thực
-  async sendUserVerification(user: User, code: string) {
-  console.log('📨 [EmailService] sendUserVerification called');
-  console.log('📨 To:', user.email);
+//   async sendUserVerification(user: User, code: string) {
+//   console.log('📨 [EmailService] sendUserVerification called');
+//   console.log('📨 To:', user.email);
+  
+//   await this.mailerService.sendMail({
+//     to: user.email,
+//     subject: 'Verify email',
+//     template: './verify',
+//     context: {
+//       name: user.email,
+//       activationCode: code,
+//     },
+//   });
+
+//   console.log('📨 MailerService.sendMail finished');
+// }
+
+async sendUserVerification(user: User, code: string) {
+  const templatePath = join(
+    process.cwd(),
+    'dist',
+    'email',
+    'templates',
+    'verify.hbs',
+  );
+
+  console.log('📁 Checking template path:', templatePath);
+  console.log('📁 Exists?', fs.existsSync(templatePath));
+
   await this.mailerService.sendMail({
     to: user.email,
     subject: 'Verify email',
-    text: `Your verification code is: ${code}`,
+    template: 'verify',
+    context: {
+      name: user.email,
+      activationCode: code,
+    },
   });
-  // await this.mailerService.sendMail({
-  //   to: user.email,
-  //   subject: 'Verify email',
-  //   template: './verify',
-  //   context: {
-  //     name: user.email,
-  //     activationCode: code,
-  //   },
-  // });
-
-  console.log('📨 MailerService.sendMail finished');
 }
-
-
   // Gửi email đặt lại mật khẩu
   async sendPasswordReset(user: User, resetCode: string) {
     await this.mailerService.sendMail({
@@ -43,4 +62,5 @@ export class EmailService {
   }
 
 }
+
 
