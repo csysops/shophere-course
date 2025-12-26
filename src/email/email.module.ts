c6accab356423ee4d879a4246a7a8b26125f1d21
Pham Dat <122384368+csysops@users.noreply.github.com>
2025-12-27 03:17:19 +0700

    Update email.module.ts

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MailerModule } from '@nestjs-modules/mailer';
 import { EmailService } from './email.service';
 
 @Module({
   providers: [EmailService],
-  exports: [EmailService],
+  exports: [EmailService], 
 })
 export class EmailModule {}
